7cb1fd4235326a1d2071384b774e72a072271e54
thosijulio <thosijulio@gmail.com>
2025-06-20 13:43:14 -0300

    FEAT - title and vsl

---
+++ app/components/titleAndVSL.tsx
@@ -0,0 +1,55 @@
+import "./titleAndVSL.css";
+
+const TitleAndVSL = () => {
+  return (
+    <div id="title-and-vsl-container">
+      <div id="title-and-vsl-wrapper">
+        <div id="title-wrapper">
+          <h2 class="elementor-heading-title elementor-size-default">
+            Descubra com dois cliques{" "}
+            <span id="“des”">
+              os criativos que grandes players estão escalando nesse exato
+              momento
+            </span>{" "}
+            nos funis mais lucrativos do BR, EUA e LATAM
+          </h2>
+        </div>
+        <div>
+          <script type="text/javascript">
+            {
+              "var s=document.createElement('script');s.src='https://scripts.converteai.net/lib/js/smartplayer/v1/sdk.min.js',s.setAttribute('data-id', '6852f9583be2bca68d08f0ac'),s.async=!0,document.head.appendChild(s);"
+            }
+          </script>
+          <div
+            id="ifr_6852f9583be2bca68d08f0ac_wrapper"
+            style={{ margin: "0 auto", width: "100%" }}
+          >
+            {" "}
+            <div
+              style={{ padding: "56.25% 0 0 0", position: "relative" }}
+              id="ifr_6852f9583be2bca68d08f0ac_aspect"
+            >
+              {" "}
+              <iframe
+                frameBorder="0"
+                allowFullScreen={true}
+                src="https://scripts.converteai.net/50168b72-b90c-4307-88b1-05e70a16f603/players/6852f9583be2bca68d08f0ac/embed.html"
+                id="ifr_6852f9583be2bca68d08f0ac"
+                style={{
+                  position: "absolute",
+                  top: 0,
+                  left: 0,
+                  width: "100%",
+                  height: "100%",
+                }}
+                referrerPolicy="origin"
+              ></iframe>{" "}
+            </div>{" "}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TitleAndVSL;
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Header from "./components/header";
+import TitleAndVSL from "./components/titleAndVSL";
 import "./page.css";
 
 export default function Home() {
   return (
     <div id="page">
       <Header />
+      <TitleAndVSL />
     </div>
   );
 }
