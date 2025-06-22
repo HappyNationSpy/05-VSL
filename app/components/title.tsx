09e814b6e6af7fd4aba4d389598a63105da477ec
thosijulio <thosijulio@gmail.com>
2025-06-22 11:34:02 -0300

    ignore file

---
+++ app/components/title.tsx
@@ -0,0 +1,24 @@
+import "./title.css";
+
+const TitleAndVSL = () => {
+  return (
+    <div id="title-container">
+      <div id="title-wrapper">
+        <div>
+          <div>
+            <h2>
+              Descubra com dois cliques{" "}
+              <span id="“des”">
+                os criativos que grandes players estão escalando nesse exato
+                momento
+              </span>{" "}
+              nos funis mais lucrativos do BR, EUA e LATAM
+            </h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TitleAndVSL;
