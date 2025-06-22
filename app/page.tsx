2ccda9dfb398f5700deaceb0907b822bc42d57cb
thosijulio <thosijulio@gmail.com>
2025-06-22 06:30:37 -0300

    ignore file

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,14 @@
-import dynamic from "next/dynamic";
 import Header from "./components/header";
 import TitleAndVSL from "./components/titleAndVSL";
-
+import VideoContainer from "./components/videoContainer";
 import "./page.css";
-
-const DynamicVideo = dynamic(() => import("./components/Video"));
 
 export default function Home() {
   return (
     <div id="page">
       <Header />
       <TitleAndVSL />
-      <div
-        id="vsl-player-container"
-        style={{
-          margin: "0 auto",
-          width: "100%",
-          position: "relative",
-          marginTop: "-200px",
-          zIndex: 1,
-        }}
-      >
-        <DynamicVideo />
-      </div>
+      <VideoContainer />
     </div>
   );
 }
