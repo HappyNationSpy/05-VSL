ae5cdd3d1286b939b1325de14d7f951ddde7c9ec
thosijulio <thosijulio@gmail.com>
2025-06-22 07:19:02 -0300

    ignore file

--- app/components/videoContainer.tsx
+++ app/components/videoContainer.tsx
@@ -1,8 +1,24 @@
+"use client";
+
+import { useState, useEffect } from "react";
 import dynamic from "next/dynamic";
 
 const DynamicVideo = dynamic(() => import("./Video"));
 
 const VideoContainer = () => {
+  // Estado para controlar se devemos mostrar o vídeo
+  const [shouldShowVideo, setShouldShowVideo] = useState(false);
+
+  useEffect(() => {
+    // Estratégia de Atraso: Espera 3 segundos após o carregamento da página
+    const timer = setTimeout(() => {
+      setShouldShowVideo(true);
+    }, 3000); // 3 segundos de atraso
+
+    // Limpa o timer se o componente for desmontado
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <div
       id="vsl-player-container"
@@ -14,7 +30,8 @@ const VideoContainer = () => {
         zIndex: 1,
       }}
     >
-      <DynamicVideo />
+      {/* Só renderiza o vídeo quando shouldShowVideo for true */}
+      {shouldShowVideo && <DynamicVideo />}
     </div>
   );
 };
