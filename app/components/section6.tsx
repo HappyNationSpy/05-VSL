7642629bd27ad49ee00c60a09b5df1a78b0d2672
thosijulio <thosijulio@gmail.com>
2025-06-24 16:23:51 -0300

    FEAT - animacao sessao 5

---
+++ app/components/section6.tsx
@@ -0,0 +1,13 @@
+import "./section6.css";
+
+const Section6 = () => {
+  return (
+    <div id="section-6-container">
+      <div id="section-6-container-wrapper">
+        <div id="section-6-container-content"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Section6;
