c1363bd78d8c4054a2f78c0981903ec86bfd7dee
thosijulio <thosijulio@gmail.com>
2025-06-24 17:03:44 -0300

    FEAT - sessao 6

--- app/components/section6.tsx
+++ app/components/section6.tsx
@@ -1,10 +1,110 @@
+import Image from "next/image";
 import "./section6.css";
 
 const Section6 = () => {
   return (
     <div id="section-6-container">
       <div id="section-6-container-wrapper">
-        <div id="section-6-container-content"></div>
+        <div id="section-6-container-content">
+          <div>
+            <h2>Happy Nation Spy?</h2>
+          </div>
+          <div>
+            <p>
+              <i>
+                <span>
+                  Sim, existe uma forma nova, simples e inteligente de encontrar
+                  criativos lucrativos no tráfego direto.
+                </span>
+              </i>
+            </p>
+          </div>
+          <div>
+            <p>
+              <span>A maioria ainda tenta escalar campanhas </span>
+              <span>
+                <b>baseado em tentativa e erro</b>
+              </span>
+              <span>
+                {" "}
+                — gastando tempo e verba testando criativos aleatórios.
+              </span>
+            </p>
+            <p>
+              <span>Mas existe uma nova forma de escalar no tráfego pago:</span>
+              <span>
+                <br />
+              </span>{" "}
+              <span>
+                <b>
+                  espionar o que grandes players estão testando e escalando
+                  agora
+                </b>
+              </span>
+              <span>, com clareza e velocidade.</span>
+            </p>
+            <p>
+              <span>Essa é a proposta do Happy Nation Spy:</span>
+              <span>
+                <br />
+              </span>
+              <span> Te mostrar </span>
+              <span>
+                <b>quais criativos têm maior chance de dar certo</b>
+              </span>
+              <span>, antes de você gastar dinheiro neles.</span>
+            </p>
+            <p>
+              <i>
+                <span>O Happy Nation Spy foi feito pra quem está </span>
+              </i>
+              <span>
+                <b>
+                  <i>no campo de batalha do tráfego direto</i>
+                </b>
+              </span>
+              <i>
+                <span>, todos os dias.</span>
+              </i>
+            </p>
+            <p>
+              <i>
+                <span>E acredite: </span>
+              </i>
+              <span>
+                <b>
+                  <i>
+                    é isso que os caras que vendem Nutra e Info pesado estão
+                    usando agora.
+                  </i>
+                </b>
+              </span>
+              <b>
+                <i>
+                  <br />
+                </i>
+              </b>
+              <i>
+                <span>
+                  {" "}
+                  Se você quer competir com eles, precisa enxergar o jogo com os
+                  mesmos olhos.
+                </span>
+              </i>
+            </p>
+          </div>
+          <div>
+            {" "}
+            <Image
+              loading="lazy"
+              decoding="async"
+              width="768"
+              height="600"
+              src="/print-Editado-768x600.webp"
+              alt="notebook"
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
