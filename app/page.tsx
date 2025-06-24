b0bac6cd54aa065032710776aa721843109f7974
thosijulio <thosijulio@gmail.com>
2025-06-24 14:48:35 -0300

    FEAT - sessão 5

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Header from "./components/header";
 import Section2 from "./components/section2";
 import Section3 from "./components/section3";
-import Section4 from "./components/section4";
+// import Section4 from "./components/section4";
 //import Section5Card1 from "./components/section5-card-1";
 import TitleAndVSL from "./components/title";
 import VideoContainer from "./components/videoContainer";
@@ -15,7 +15,7 @@ export default function Home() {
       <VideoContainer />
       <Section2 />
       <Section3 />
-      <Section4 />
+      {/* <Section4 /> */}
     </div>
   );
 }
