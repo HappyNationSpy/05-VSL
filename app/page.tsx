633057d68138300c62a010d85134015b949ace5e
thosijulio <thosijulio@gmail.com>
2025-06-24 14:54:56 -0300

    FEAT - imagem

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+// import dynamic from "next/dynamic";
 import Header from "./components/header";
 import Section2 from "./components/section2";
 import Section3 from "./components/section3";
@@ -7,6 +9,8 @@ import Title from "./components/title";
 import VideoContainer from "./components/videoContainer";
 import "./page.css";
 
+// const DynamicSection4 = dynamic(() => import("./components/section4"));
+
 export default function Home() {
   return (
     <div id="page">
@@ -15,7 +19,7 @@ export default function Home() {
       <VideoContainer />
       <Section2 />
       <Section3 />
-      {/* <Section4 /> */}
+      {/* <DynamicSection4 /> */}
     </div>
   );
 }
