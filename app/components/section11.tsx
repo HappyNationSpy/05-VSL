c50a5cdda747c1e8725dbb50c781d5015fce3cbb
thosijulio <thosijulio@gmail.com>
2025-06-25 15:40:22 -0300

    FEAT - sessao 11

--- app/components/section11.tsx
+++ app/components/section11.tsx
@@ -10,9 +10,49 @@ const Section11 = () => {
             O valor total de tudo o que você recebe é R$ 408,00. Mas você não
             vai pagar isso, embora valha.{" "}
           </h2>
-          <div></div>
-          <h2>No total, tudo isso deveria custar:</h2>
-          <h1>R$ 408</h1>
+          <div>
+            <h2>
+              Seu investimento total para ter ACESSO A TODOS ESSES MATERIAIS é:
+            </h2>
+          </div>
+          <div>
+            <div>
+              <p>De R$ 408</p>
+              <p>
+                Por
+                <br /> apenas
+                <br />
+                <span>
+                  <strong>R$ 97</strong>
+                </span>
+              </p>
+              <ul>
+                <li>Sem taxa de ativação.</li>
+                <li>Sem fidelidade.</li>
+                <li>Com acesso imediato e vantagem injusta.</li>
+              </ul>
+              <div>
+                <a
+                  rel="noopener"
+                  href="https://pay.kiwify.com.br/OV4dt7C"
+                  target="_blank"
+                >
+                  <span>
+                    <span>SIM, EU QUERO ESCALAR COM VANTAGEM!</span>
+                    <p>
+                      <svg
+                        aria-hidden="true"
+                        viewBox="0 0 448 512"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path d="M190.5 66.9l22.2-22.2c9.4-9.4 24.6-9.4 33.9 0L441 239c9.4 9.4 9.4 24.6 0 33.9L246.6 467.3c-9.4 9.4-24.6 9.4-33.9 0l-22.2-22.2c-9.5-9.5-9.3-25 .4-34.3L311.4 296H24c-13.3 0-24-10.7-24-24v-32c0-13.3 10.7-24 24-24h287.4L190.9 101.2c-9.8-9.3-10-24.8-.4-34.3z"></path>
+                      </svg>{" "}
+                    </p>
+                  </span>
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
       <div className="breaker-line-orange" />
